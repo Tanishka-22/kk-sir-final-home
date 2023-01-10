a7bb2de0be115202a4d817f1fd482fd51d99b5e6
KunalMehra075 <112753481+KunalMehra075@users.noreply.github.com>
2023-01-10 11:01:12 +0530

    Kunal_VersionX

--- home done/index.js
+++ home done/index.js
@@ -2,3 +2,20 @@ var loader = document.querySelector("#preloader");
 window.addEventListener("load", function () {
   loader.style.display = "none";
 });
+
+document.addEventListener("click", (e) => {
+  const ddbtn = e.target.matches("[data-dropdown-button]");
+  console.log(dropmeow.dataset.visible);
+  if (!ddbtn) {
+    dropmeow.style.display = "none";
+    dropmeow.dataset.visible = "X";
+  } else {
+    if (dropmeow.dataset.visible == "Y") {
+      dropmeow.style.display = "none";
+      dropmeow.dataset.visible = "X";
+    } else {
+      dropmeow.style.display = "block";
+      dropmeow.dataset.visible = "Y";
+    }
+  }
+});
